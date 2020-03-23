ac233a90b2546e54aa0ddb93bb51d13fc13aa709
Mandy <drgnfli6420@yahoo.com>
2020-03-23 16:53:31 -0400

    loops

---
+++ script.js
@@ -0,0 +1,25 @@
+// In your JavaScript file, create an array of 5 of your interests.
+// Loop through the interests and console log the phrase "One of my interests is: [your interest]." for each of the interests in your array.
+// Output:
+// One of my interests is: hiking.
+// One of my interests is: coding.
+// One of my interests is: math.
+// One of my interests is: 30 Rock.
+// One of my interests is: pizza.
+
+// var interests = ["hiking", "snuggling with my dog", "eating", "hanging out with people", "baking cookies", "watching TV"
+
+// // console.log(`One of my interests is: ${interests[0]}`)
+// // console.log(`One of my interests is: ${interests[1]}`)
+// // // console.log(`One of my interests is: ${interests[2]}`)
+
+// for(var i = 0; i < interests.length; i++){
+//     // Everything in here happens over and over again
+//     console.log(`One of my interests is: ${interests[i]}`)
+//     console.log("this is i", i)""
+
+//}
+var interests = ["watching tv", "Playing with my son", "chatting with friends"]
+    for(var i = 0; i < interests.length; i++){
+        console.log(`One of my interests is: ${interests[i]}`)
+    }
